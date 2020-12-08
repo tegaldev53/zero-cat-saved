d12b5860666ef7c6a9d2bac00dbf265eaf6c3951
umaralfaruqdev <ualfaruq59@gmail.com>
2020-12-08 21:35:00 +0700

    moment

--- App.js
+++ App.js
@@ -50,7 +50,7 @@ const App = async () => {
     console.timeEnd('render-init-page');
 
     // timout
-    await Timeout('31:50');
+    await Timeout('36:50');
 
     // reload
     console.time('render product page');
